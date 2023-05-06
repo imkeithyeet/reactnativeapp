494fef9aaeb5bf822184b7ef2c7b2763b3fb2ac5
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-05-06 14:28:05 -0700

    new

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fuck bitches get ass!</Text>
+      <Text>This is our first project</Text>
+      <Text>- Code Palace</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#63c5da',
     alignItems: 'center',
     justifyContent: 'center',
   },
